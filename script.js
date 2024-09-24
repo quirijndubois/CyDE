ca5b674f1fe618971798971a7bb84419b7895b74
Quirijn du Bois <quirijndubois@gmail.com>
2024-09-24 15:48:33 +0200

    Added some default text

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
 
-// script.js
+document.getElementById('MathJax-script').addEventListener('load', function() {
+    defaultText = 'The schrodinger equation: \\[ -\\frac{\\hbar^2}{2m} \\frac{d^2 \\psi}{dx^2} + V\\psi = E\\psi\\\\ \\]'
+    document.getElementById('latex-input').value = defaultText;
+    document.getElementById('latex-preview').innerHTML = defaultText;
+    MathJax.typesetPromise();
+  });
+
 document.getElementById('latex-input').addEventListener('input', function() {
     const latexInput = this.value;
     
-    // Set the content of the preview
     document.getElementById('latex-preview').innerHTML = latexInput;
-
+    
+    MathJax.typesetPromise();
 });
+
