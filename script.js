7376c3b9ca51bd83e47ec11c44e5762db07d004f
Quirijn du Bois <quirijndubois@gmail.com>
2024-09-24 16:42:13 +0200

    Fixed multiline string

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 
 document.getElementById('MathJax-script').addEventListener('load', function() {
-    defaultText = 'The schrodinger equation: \\[ -\\frac{\\hbar^2}{2m} \\frac{d^2 \\psi}{dx^2} + V\\psi = E\\psi\\\\ \\]'
+    let defaultText =
+`The schrodinger equation: 
+\\[
+-\\frac{\\hbar^2}{2m} \\frac{d^2 \\psi}{dx^2} + V\\psi = E\\psi\\\\
+\\]`
+
     document.getElementById('latex-input').value = defaultText;
     document.getElementById('latex-preview').innerHTML = defaultText;
     MathJax.typesetPromise();
