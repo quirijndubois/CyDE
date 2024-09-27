ee1cd9182f3bf0c80cd1a0aaa9c51b51e49961e2
Quirijn du Bois <quirijndubois@gmail.com>
2024-09-27 17:57:23 +0200

    Added filebrowser ui

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 
 
-document.getElementById('MathJax-script').addEventListener('load', function() {
+document.getElementById('MathJax-script').addEventListener('load', function () {
     let defaultText =
-`The schrodinger equation: 
+        `The schrodinger equation: 
 \\[
 -\\frac{\\hbar^2}{2m} \\frac{d^2 \\psi}{dx^2} + V\\psi = E\\psi\\\\
 \\]`
 
-    document.getElementById('latex-input').value = defaultText;
-    document.getElementById('latex-preview').innerHTML = defaultText;
-    MathJax.typesetPromise();
-  });
-
-document.getElementById('latex-input').addEventListener('input', function() {
-    const latexInput = this.value;
-    
-    document.getElementById('latex-preview').innerHTML = latexInput;
-    
+    document.getElementById('code-field').value = defaultText;
+    document.getElementById('preview-field').innerHTML = defaultText;
     MathJax.typesetPromise();
 });
+
+document.getElementById('code-field').addEventListener('input', function () {
+    const latexInput = this.value;
+
+    document.getElementById('preview-field').innerHTML = latexInput;
+
+    MathJax.typesetPromise();
+});
+
+// Get all file elements
+const files = document.querySelectorAll('.file');
+
+// Add an onclick event listener to each file element
+files.forEach((file) => {
+    file.addEventListener('click', () => {
+        // Remove the 'selected' class from all files
+        files.forEach((f) => f.classList.remove('selected'));
+
+        // Add the 'selected' class to the clicked file
+        file.classList.add('selected');
+    });
+});
+
 
